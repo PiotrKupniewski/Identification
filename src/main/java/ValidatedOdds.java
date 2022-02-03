cf08ac1e1813a72687967c43020e16ad7e3c4fdc
pkupniewski <p.kupniewski@sportradar.com>
2022-02-03 19:02:45 +0100

    Validated odds object added

--- src/main/java/ValidatedOdds.java
+++ src/main/java/ValidatedOdds.java
@@ -1,2 +1,18 @@
 public class ValidatedOdds {
+
+  private final String id;
+  private final Double odds;
+
+  public ValidatedOdds(String id, Double odds) {
+    this.id = id;
+    this.odds = odds;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public Double getOdds() {
+    return odds;
+  }
 }
