6ddb5e0612347ca887b962ddfb1439e10732150b
pkupniewski <p.kupniewski@sportradar.com>
2022-02-03 19:01:58 +0100

    Identification core added

--- src/main/java/Identification.java
+++ src/main/java/Identification.java
@@ -1,2 +1,27 @@
+import java.util.List;
+
 public class Identification {
+
+  private final RawOddsListener listener;
+  private final Validator validator;
+  private final OutputCreator outputCreator;
+
+  public Identification(
+      RawOddsListener listener, Validator validator, OutputCreator outputCreator) {
+    this.listener = listener;
+    this.validator = validator;
+    this.outputCreator = outputCreator;
+  }
+
+  public List<RawOdds> startListening() {
+    return listener.listenOnStream();
+  }
+
+  public List<ValidatedOdds> validate(List<RawOdds> rawOdds) {
+    return validator.validate(rawOdds);
+  }
+
+  public List<OddsWrapper> createOutput(List<ValidatedOdds> validatedOdds) {
+    return outputCreator.create(validatedOdds);
+  }
 }
